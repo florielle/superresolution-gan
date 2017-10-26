2af4e3a60ec2106e8f9f4cd977fc7a0664122baa
florielle <jmw784@nyu.edu>
2017-10-26 01:40:42 -0400

    Adding model file and first cut of train script

--- model.py
+++ model.py
@@ -1,21 +1,51 @@
-import os
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
-import torch.utils.data as data
-from torch.autograd import Variable
-from PIL import Image
-import numpy as np
-from dataloader import srData, pil_loader, name_list
 
-lr_dir = "/scratch/jmw784/superresolution/data/lr/"
-hr_dir = "/scratch/jmw784/superresolution/data/hr/"
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+class SRGAN_D(nn.Module):
+    def __init__(self, ngpu):
+        super(SRGAN_D, self).__init__()
+        self.ngpu = ngpu
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout()
+        self.conv1 = nn.Conv2d(3, 64, 4, 2, 1, bias=True)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 32, 4, 2, 1, bias=True)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 16, 4, 2, 1, bias=True)
+        self.bn3 = nn.BatchNorm2d(16)
+        self.conv4 = nn.Conv2d(16, 8, 4, 2, 1, bias=True)
+        self.bn4 = nn.BatchNorm2d(8)
+        self.linear = nn.Linear(8*16*16, 1)
 
-dataset = srData(lr_dir, hr_dir, transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+    def forward(self, x):
+        x = self.relu(self.dropout(self.bn1(self.conv1(x))))
+        x = self.relu(self.dropout(self.bn2(self.conv2(x))))
+        x = self.relu(self.dropout(self.bn3(self.conv3(x))))
+        x = self.relu(self.dropout(self.bn4(self.conv4(x))))
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
 
-print(dataloader.dataset[0])
+class SRGAN_G(nn.Module):
+    def __init__(self, ngpu):
+        super(SRGAN_G, self).__init__()
+        self.ngpu = ngpu
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout()
+        self.conv1 = nn.Conv2d(3, 64, 5, 1, 2, bias=True)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, 3, 1, 1, bias=True)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 32, 3, 1, 1, bias=True)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.conv4 = nn.Conv2d(32, 3*4 ** 2, 3, 1, 1, bias=True)
+        self.pixel_shuffle = nn.PixelShuffle(4)
+        self.tanh = nn.Tanh()
 
+    def forward(self, x):
+        x = self.relu(self.dropout(self.bn1(self.conv1(x))))
+        x = self.relu(self.dropout(self.bn2(self.conv2(x))))
+        x = self.relu(self.dropout(self.bn3(self.conv3(x))))
+        x = self.relu(self.dropout(self.pixel_shuffle(self.conv4(x))))
+        x = self.tanh(x)
+        return x
---
+++ train.py
@@ -0,0 +1,233 @@
+import argparse
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import torch.nn.init as init
+from torch.autograd import Variable
+import os
+import numpy as np
+from PIL import Image
+from dataloader import *
+from model import *
+
+"""
+Options for training
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nc', type=int, default=3, help='input image channels')
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
+parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
+parser.add_argument('--ngpu'  , type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--clamp_lower', type=float, default=-0.01)
+parser.add_argument('--clamp_upper', type=float, default=0.01)
+parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
+parser.add_argument('--experiment', default=None, help='Where to store samples and models')
+parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
+parser.add_argument('--init', type=str, default='normal', help='initialization method (normal, xavier, kaiming)')
+opt = parser.parse_args()
+print(opt)
+
+
+"""
+Load experiments 
+"""
+if opt.experiment is None:
+    opt.experiment = 'samples'
+os.system('mkdir {0}'.format(opt.experiment))
+os.system('mkdir {0}/images'.format(opt.experiment))
+
+opt.manualSeed = random.randint(1, 10000) # fix seed
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+cudnn.benchmark = True
+
+###############################################################################
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+lr_dir = "/scratch/jmw784/superresolution/data/lr/"
+hr_dir = "/scratch/jmw784/superresolution/data/hr/"
+dataset = srData(lr_dir, hr_dir, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+                                         shuffle=True, num_workers=int(opt.workers),
+                                         drop_last=True)
+
+ngpu = int(opt.ngpu)
+nc = int(opt.nc)
+
+# custom weights initialization called on netG and netD
+
+if opt.init not in ['normal', 'xavier', 'kaiming']:
+    print('Initialization method not found, defaulting to normal')
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        if opt.init == 'xavier':
+            m.weight.data = init.xavier_normal(m.weight.data)
+            m.bias.data = init.xavier_normal(m.bias.data)
+        elif opt.init == 'kaiming':
+            m.weight.data = init.kaiming_normal(m.weight.data)
+            m.bias.data = init.kaiming_normal(m.bias.data)
+        else:
+            m.weight.data.normal_(0.0, 0.02)
+            m.bias.data.fill_(0)
+
+    elif classname.find('BatchNorm') != -1:
+        if opt.init == 'xavier':
+            m.weight.data = init.xavier_normal(m.weight.data)
+            m.bias.data = init.xavier_normal(m.bias.data)
+        elif opt.init == 'kaiming':
+            m.weight.data = init.kaiming_normal(m.weight.data)
+            m.bias.data = init.kaiming_normal(m.bias.data)
+        else:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.fill_(0)
+
+# Create model objects
+
+netG = SRGAN_G(opt.imageSize, nc, ngpu)
+netG.apply(weights_init)
+netG.train()
+
+netD = SRGAN_D(opt.imageSize, nc, ngpu)
+netD.apply(weights_init)
+netD.train()
+
+# Define MSE loss module
+MSE = nn.MSELoss()
+
+# Load checkpoint models if needed
+
+if opt.netG != '': 
+    netG.load_state_dict(torch.load(opt.netG))
+print(netG)
+
+if opt.netD != '':
+    netD.load_state_dict(torch.load(opt.netD))
+print(netD)
+
+
+# Predefine tensor sizes and turn to cuda if needed
+
+lr_input = torch.FloatTensor(opt.batchSize, nc, opt.lrSize, opt.lrSize)
+hr_target = torch.FloatTensor(opt.batchSize, nc, opt.hrSize, opt.hrSize)
+one = torch.FloatTensor([1])
+mone = one * -1
+
+if opt.cuda:
+    netD.cuda()
+    netG.cuda()
+    lr_img = lr_img.cuda()
+    hr_img = hr_img.cuda()
+    noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+
+# Set up optimizer
+
+if opt.adam:
+    optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999))
+else:
+    optimizerD = optim.RMSprop(netD.parameters(), lr = opt.lrD)
+    optimizerG = optim.RMSprop(netG.parameters(), lr = opt.lrG)
+
+gen_iterations = 0
+for epoch in range(opt.niter+1):
+    data_iter = iter(dataloader)
+    i = 0
+    while i < len(dataloader):
+        ############################
+        # (1) Update D network
+        ###########################
+        for p in netD.parameters(): # reset requires_grad
+            p.requires_grad = True # they are set to False below in netG update
+
+        # train the discriminator Diters times
+        if gen_iterations < 25 or gen_iterations % 500 == 0:
+            Diters = 100
+        else:
+            Diters = opt.Diters
+        j = 0
+
+        while j < Diters and i < len(dataloader):
+            j += 1
+
+            lr, hr = data_iter.next()
+            i += 1
+
+            # train with real
+
+            netD.zero_grad()
+
+            lr_input.resize_as_(lr).copy_(lr)
+            hr_target.resize_as_(hr).copy_(hr)
+
+            inputv = Variable(lr_input)
+            inputy = Variable(hr_target)
+
+            errD_real = netD(inputy) # can modify to feed inputv too
+
+            # freeze netG while we train the discriminator
+            inputg = Variable(lr_input, volatile = True)
+            fake = Variable(netG(inputg).data)
+
+            errD_fake = netD(fake)
+
+            # calculate discriminator loss and MSE loss and add together
+            errD = 0.5 * (torch.mean((errD_real - 1)**2) + torch.mean(errD_fake**2))
+            loss_D = errD + MSE(fake, inputy)
+
+            # backprop the loss
+            loss.backward()                            
+            
+            optimizerD.step()
+
+        ############################
+        # (2) Update G network
+        ###########################
+        for p in netD.parameters():
+            p.requires_grad = False # to avoid computation
+        netG.zero_grad()
+
+        inputv = Variable(lr_input)
+        
+        fake = netG(inputv)
+        errG_1 = netD(fake)
+        errG = 0.5 * torch.mean((errG_1 - 1)**2)
+
+        loss_G = errG + MSE(fake, inputy)
+        loss_G.backward()
+
+        optimizerG.step()
+        gen_iterations += 1
+
+        print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f'
+            % (epoch, opt.niter, i, len(dataloader), gen_iterations,
+            errD.data[0], errG.data[0]))
+        if gen_iterations % 500 == 0:
+            vutils.save_image(hr, '{0}/images/{1}_real.png'.format(opt.experiment, gen_iterations))
+            fake = netG(Variable(lr, volatile=True))
+            vutils.save_image(fake.data, '{0}/images/{1}_fake.png'.format(opt.experiment, gen_iterations))
+
+    if epoch % 200 == 0:
+    # do checkpointing
+        torch.save(netG.state_dict(), '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
+        torch.save(netD.state_dict(), '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
